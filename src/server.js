2e2d55512af298e35566caec01de8daf5aee77a2
joseVitor03 <jv681033@gmail.com>
2024-01-02 10:33:42 -0300

    variavel de ambiente port adicionada

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 // src/server.js
 const app = require('./app');
+const port = process.env.PORT || 3003;
 
-app.listen(3003, () => console.log('server running on port 3003'));
+app.listen(port, () => console.log('server running on port 3003'));
